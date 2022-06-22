a88df2dd682a539b232e87f8a2889f42a1b1b933
Leon Kruslin <leon.kruslin@gmail.com>
2022-06-22 16:04:49 +0200

    Listing all apartments with ajax

--- RwaApartments-Public/Controllers/ApartmentsController.cs
+++ RwaApartments-Public/Controllers/ApartmentsController.cs
@@ -11,6 +11,7 @@ namespace RwaApartments_Public.Controllers
 {
     public class ApartmentsController : Controller
     {
+        [Obsolete]
         [HttpGet]
         public ActionResult BrowseApartments(string search, int? cityId, int? statusId, string filterCode)
         {
@@ -44,16 +45,49 @@ namespace RwaApartments_Public.Controllers
         }
 
         [HttpGet]
+        public ActionResult LoadApartmentListPartialView(string search, int? cityId, int? statusId, string filterCode)
+        {
+            Predicate<Apartment> avaliabilityFilter = (a => a.IsAvaliable);
+            Predicate<Apartment> nameFilter = (a => true);
+            Predicate<Apartment> cityFilter = (a => true);
+            Predicate<Apartment> statusFilter = (a => true);
+
+            if (search != null && search != string.Empty)
+                nameFilter = (a => a.NameEng.Contains(search));
+            if (cityId != null && cityId != 0)
+                cityFilter = (a => a.CityId.Equals(cityId));
+            if (statusId != null && statusId != 0)
+                statusFilter = (a => a.StatusId.Equals(statusId));
+
+            List<Apartment> apartments = (List<Apartment>)RepoFactory.GetRepoInstance().LoadApartments(avaliabilityFilter, nameFilter, cityFilter, statusFilter);
+
+            if (filterCode != null && filterCode != string.Empty)
+            {
+                if (Apartment.ComparisonDicitionary.Keys.Any(key => key.Equals(filterCode)))
+                    apartments.Sort(Apartment.ComparisonDicitionary[filterCode]);
+            }
+            else
+                apartments.Sort(Apartment.PriceLowToHighComparison);
+
+            var model = new ApartmentListViewModel
+            {
+                Apartments = apartments
+            };
+            return PartialView("_ApartmentListView", model);
+        }
+
+        [HttpGet]
+        public ActionResult DisplayApartmentInListView(int id)
+        {
+            var model = RepoFactory.GetRepoInstance().LoadApartmentById(id);
+            return PartialView(viewName: "_ApartmentInListView", model: model);
+        }
+
+        [HttpGet]
         public ActionResult ViewApartment(int id)
         {
             var model = RepoFactory.GetRepoInstance().LoadApartmentById(id);
             return View(model);
-        }
-
-        [HttpGet]
-        public ActionResult Hehe(int id)
-        {
-            return Content(id.ToString());
         }
     }
 }
