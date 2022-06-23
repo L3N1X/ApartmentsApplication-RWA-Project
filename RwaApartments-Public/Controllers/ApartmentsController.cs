9a170f1291735947b56ff758ececbe5a3fd6cadc
Leon Kruslin <leon.kruslin@gmail.com>
2022-06-23 16:36:44 +0200

    Ajax call after dropdown list changes

--- RwaApartments-Public/Controllers/ApartmentsController.cs
+++ RwaApartments-Public/Controllers/ApartmentsController.cs
@@ -11,41 +11,14 @@ namespace RwaApartments_Public.Controllers
 {
     public class ApartmentsController : Controller
     {
-        [Obsolete]
         [HttpGet]
-        public ActionResult BrowseApartments(string search, int? cityId, int? statusId, string filterCode)
+        public ActionResult BrowseApartments()
         {
-            Predicate<Apartment> avaliabilityFilter = (a => a.IsAvaliable);
-            Predicate<Apartment> nameFilter = (a => true);
-            Predicate<Apartment> cityFilter = (a => true);
-            Predicate<Apartment> statusFilter = (a => true);
-
-            if(search != null && search != string.Empty)
-                nameFilter = (a => a.NameEng.Contains(search));
-            if (cityId != null && cityId != 0)
-                cityFilter = (a => a.CityId.Equals(cityId));
-            if (statusId != null && statusId != 0)
-                statusFilter = (a => a.StatusId.Equals(statusId));
-
-            List<Apartment> apartments = (List<Apartment>)RepoFactory.GetRepoInstance().LoadApartments(avaliabilityFilter, nameFilter, cityFilter, statusFilter);
-
-            if (filterCode != null && filterCode != string.Empty)
-            {
-                if (Apartment.ComparisonDicitionary.Keys.Any(key => key.Equals(filterCode)))
-                    apartments.Sort(Apartment.ComparisonDicitionary[filterCode]);
-            }
-            else
-                apartments.Sort(Apartment.PriceLowToHighComparison);
-
-            var model = new ApartmentsBrowserViewModel
-            {
-                Apartments = apartments
-            };
-            return View(model);
+            return View();
         }
 
         [HttpGet]
-        public ActionResult LoadApartmentListPartialView(string search, int? cityId, int? statusId, string filterCode)
+        public ActionResult LoadApartmentListPartialView(string search, int? cityId, string statusId, string filterCode)
         {
             Predicate<Apartment> avaliabilityFilter = (a => a.IsAvaliable);
             Predicate<Apartment> nameFilter = (a => true);
@@ -56,8 +29,13 @@ namespace RwaApartments_Public.Controllers
                 nameFilter = (a => a.NameEng.Contains(search));
             if (cityId != null && cityId != 0)
                 cityFilter = (a => a.CityId.Equals(cityId));
-            if (statusId != null && statusId != 0)
-                statusFilter = (a => a.StatusId.Equals(statusId));
+            if (statusId != null && statusId != string.Empty)
+            {
+                if ("all".Equals(statusId))
+                    avaliabilityFilter = (a => true);
+                else if ("unavaliable".Equals(statusId))
+                    avaliabilityFilter = (a => !a.IsAvaliable);
+            }
 
             List<Apartment> apartments = (List<Apartment>)RepoFactory.GetRepoInstance().LoadApartments(avaliabilityFilter, nameFilter, cityFilter, statusFilter);
 
