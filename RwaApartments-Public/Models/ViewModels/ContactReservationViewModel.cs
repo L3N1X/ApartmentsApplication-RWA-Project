cb19ce3df81cdf6f5df791aec1e9e7493ed5dfd7
Leon Kruslin <leon.kruslin@gmail.com>
2022-06-26 14:14:59 +0200

    Adding review placement

--- RwaApartments-Public/Models/ViewModels/ContactReservationViewModel.cs
+++ RwaApartments-Public/Models/ViewModels/ContactReservationViewModel.cs
@@ -12,11 +12,11 @@ namespace RwaApartments_Public.Models.ViewModels
         public User User { get; set; }
 
         [DataType(DataType.Date), Required]
-        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd}", ApplyFormatInEditMode = true)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
         public DateTime StartTime { get; set; }
 
         [DataType(DataType.Date), Required]
-        [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd}", ApplyFormatInEditMode = true)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
         public DateTime EndTime { get; set; }
     }
 }
