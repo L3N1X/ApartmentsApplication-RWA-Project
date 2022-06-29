025bbcf4b441c203f4c6753c0a09351dd2291a46
Leon Kruslin <leon.kruslin@gmail.com>
2022-06-29 20:01:37 +0200

    login styling and error page adjusted

--- RwaApartments-Public/Global.asax.cs
+++ RwaApartments-Public/Global.asax.cs
@@ -17,5 +17,10 @@ namespace RwaApartments_Public
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
+
+        protected void Application_Error()
+        {
+            Response.Redirect("~/Error/Index");
+        }
     }
 }
